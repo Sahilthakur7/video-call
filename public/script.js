d699e41843ab9afe3b8f893889c0011356fb100e
sahilthakur7 <sahilrd07@gmail.com>
2021-01-05 21:07:25 +0530

    Added video to the screen

--- public/script.js
+++ public/script.js
@@ -1,7 +1,35 @@
-const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
 
-socket.emit("join-room", ROOM_ID, 10);
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+  });
+
+const socket = io("/");
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: "4001",
+});
+
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
 
 socket.on("user-connected", (userId) => {
   console.log("user connected====", userId);
 });
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  console.log("video----", video);
+  videoGrid.append(video);
+}
